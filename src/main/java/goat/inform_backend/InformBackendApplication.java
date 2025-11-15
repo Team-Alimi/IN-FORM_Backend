3ea66728adb84961cd7a9aee7c99fb1b399d6cf2
gitue <gitue11@gmail.com>
2025-11-15 10:27:52 +0900

    docs(README.md): api 제작 체크리스트 작성

---
+++ src/main/java/goat/inform_backend/InformBackendApplication.java
@@ -0,0 +1,13 @@
+package goat.inform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformBackendApplication.class, args);
+	}
+
+}
