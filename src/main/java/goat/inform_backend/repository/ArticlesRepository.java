67a9b88ff59d6e27cca4c4ffa988a4b137bc5b60
gitue <gitue11@gmail.com>
2025-11-15 15:13:54 +0900

    feat(ArticlesRepository): ArticlesRepository 수정

--- src/main/java/goat/inform_backend/repository/ArticlesRepository.java
+++ src/main/java/goat/inform_backend/repository/ArticlesRepository.java
@@ -1,10 +1,13 @@
 package goat.inform_backend.repository;
 
 import goat.inform_backend.entity.articles.Articles;
+import goat.inform_backend.entity.vendors.VendorType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticlesRepository extends JpaRepository<Articles,Integer>{
-
+    Page<Articles> findByVendors_VendorType(VendorType vendorType, Pageable pageable);
 }
