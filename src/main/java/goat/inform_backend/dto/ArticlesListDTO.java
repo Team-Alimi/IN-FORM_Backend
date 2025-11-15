7244b9db383ab31476262fab8397ec83b52ab86f
gitue <gitue11@gmail.com>
2025-11-15 15:41:24 +0900

    refactor(ArticlesListDTO): ArticlesListDTO 리팩토링 진행

--- src/main/java/goat/inform_backend/dto/ArticlesListDTO.java
+++ src/main/java/goat/inform_backend/dto/ArticlesListDTO.java
@@ -7,21 +7,20 @@ import java.time.LocalDate;
 @Getter
 public class ArticlesListDTO {
     private final String title;
-    private final LocalDate startDate;
-    private final LocalDate dueDate;
-    private final LocalDate createDate;
-    private final LocalDate updateDate;
-
+    private final LocalDate start_at;
+    private final LocalDate end_at;
+    private final LocalDate created;
+    private final LocalDate updated;
 
     private final String vendor_name;
     private final String category_name;
 
     public ArticlesListDTO(Articles articles) {
         this.title = articles.getTitle();
-        this.startDate = articles.getStartDate();
-        this.dueDate = articles.getDueDate();
-        this.createDate = articles.getCreateDate();
-        this.updateDate = articles.getUpdateDate();
+        this.start_at = articles.getStartDate();
+        this.end_at = articles.getDueDate();
+        this.created = articles.getCreateDate();
+        this.updated = articles.getUpdateDate();
 
         this.vendor_name = articles.getVendors().getVendorName();
         this.category_name = articles.getCategories().getCategoryName();
